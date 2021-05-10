8d7d441ec482ef3a5e43a4f4f4179ad1fc2fe06d
Nenad Jeckovic <jeckovic.ra85.2018@uns.ac.rs>
2021-05-10 13:06:40 +0200

    Izmena i validacija lekova

--- InformacioniSistemBolnice/Lekar/LekoviPage.xaml.cs
+++ InformacioniSistemBolnice/Lekar/LekoviPage.xaml.cs
@@ -27,6 +27,8 @@ namespace InformacioniSistemBolnice.Lekar
         {
             this.parent = parent;
             InitializeComponent();
+            UpdateList();
+            SastavList.Items.Clear();
         }
 
         public static LekoviPage GetPage(LekarWindow parent)
@@ -41,6 +43,65 @@ namespace InformacioniSistemBolnice.Lekar
             LekoviZaPotvrduWindow lekoviWindow = new LekoviZaPotvrduWindow();
             Application.Current.MainWindow = lekoviWindow;
             lekoviWindow.Show();
+        }
+
+        private void DodajSastojakClick(object sender, RoutedEventArgs e)
+        {
+            if (Sastojci.SelectedIndex != -1)
+            {
+                List<Lek> lekovi = LekFileStorage.GetAll();
+                foreach (Lek l in lekovi)
+                {
+                    if (l.Naziv == LekoviList.SelectedItem.ToString())
+                    {
+                        l.ListaSastojaka.Add((Sastojak)Sastojci.SelectedItem);
+                        //dodati u storage
+                        IspisiSastojke(l);
+                    }
+                }
+            }
+        }
+
+        private void UpdateList()
+        {
+            LekoviList.Items.Clear();
+            List<Lek> lekovi = LekFileStorage.GetAll();
+            foreach (Lek l in lekovi)
+            {
+                if (!l.IsDeleted && l.StatusLeka.Equals(StatusLeka.validiran))
+                    LekoviList.Items.Add(l.Naziv);
+            }
+        }
+
+        public void SelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+            SastavList.Items.Clear();
+            foreach (Lek lek in LekFileStorage.GetAll())
+            {
+                if (lek.Naziv == LekoviList.SelectedItem.ToString())
+                {
+                    IspisiSastojke(lek);
+                }
+            }
+        }
+
+        private void IspisiSastojke(Lek lek)
+        {
+            SastavList.Items.Clear();
+            List<Sastojak> sastojci = new List<Sastojak>();
+            foreach (Sastojak sastojak in SastojakFileStorage.GetAll())
+            {
+                if (lek.ListaSastojaka.Contains(sastojak))
+                {
+                    SastavList.Items.Add(sastojak.naziv);
+                }
+                else
+                {
+                    sastojci.Add(sastojak);
+                }
+
+            }
+            Sastojci.ItemsSource = sastojci;
         }
     }
 }
