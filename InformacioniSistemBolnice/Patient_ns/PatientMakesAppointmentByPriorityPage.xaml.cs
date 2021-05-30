897e89d85d2d8020186f79c1532e440ae78a2570
Jelena Radman <jelenaradman12@uns.ac.rs>
2021-05-30 08:31:48 +0200

    Preimenovanje stranica

---
+++ InformacioniSistemBolnice/Patient_ns/PatientMakesAppointmentByPriorityPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InformacioniSistemBolnice.Patient_ns
+{
+    /// <summary>
+    /// Interaction logic for PatientMakesAppointmentByPriorityPage.xaml
+    /// </summary>
+    public partial class PatientMakesAppointmentByPriorityPage : Page
+    {
+        private StartPatientWindow parent;
+        public PatientMakesAppointmentByPriorityPage(StartPatientWindow pp)
+        {
+            parent = pp;
+            InitializeComponent();
+            parent.UpdateVisibilityOfComponents();
+            parent.titleLabel.Visibility = Visibility.Hidden;
+            parent.titlePriorityLabel.Visibility = Visibility.Visible;
+            parent.titlePriorityLabel.Content = "Izaberite prioritet pretrage";
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            parent.titlePriorityLabel.Visibility = Visibility.Hidden;
+            parent.startWindow.Content = new PatientMakesAppointmentPage(parent);
+        }
+
+        private void doctorRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PriorityDoctorPage(parent, this);
+        }
+
+        private void timeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PriorityTimePage(parent, this);
+        }
+    }
+}
---
+++ InformacioniSistemBolnice/Patient_ns/StartPatientPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InformacioniSistemBolnice.Patient_ns
+{
+    /// <summary>
+    /// Interaction logic for StartPatientPage.xaml
+    /// </summary>
+    public partial class StartPatientPage : Page
+    {
+        private static StartPatientWindow parent;
+
+        public StartPatientPage(StartPatientWindow p)
+        {
+            parent = p;
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        private void pregledTermina_Click(object sender, RoutedEventArgs e)
+        {
+            parent.startWindow.Content = new PatientExaminesAppointmentPage(parent);
+        }
+
+        private void obavjestenja_Click(object sender, RoutedEventArgs e)
+        {
+            parent.startWindow.Content = new NotificationPatientPage(parent);
+        }
+
+        private void ocjenjivanje_Click(object sender, RoutedEventArgs e)
+        {
+            PacijentFileStorage.ProvjeritiStatusPacijenta(parent.Pacijent);
+            if (parent.Pacijent.Banovan == true)
+            {
+                MessageBox.Show("Ova funkcionalnost Vam je trenutno onemogućena,obratite se sekretaru!", "Greška");
+            }
+            else
+            {
+                parent.startWindow.Content = new RatingPage(parent);
+            }
+        }
+
+        private void karton_Click(object sender, RoutedEventArgs e)
+        {
+            parent.startWindow.Content = new PatientMedicalRecordPage(parent);
+        }
+    }
+
+}
