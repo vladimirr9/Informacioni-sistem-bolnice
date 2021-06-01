3ff2e42afd0ac5444df00ce380d2ee1e2c8fe449
Vladimir <rokvic.ra69.2018@uns.ac.rs>
2021-06-01 14:03:12 +0200

    Dodavanje mnemonika i akceleratora

---
+++ InformacioniSistemBolnice/Commands/RoutedCommands.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace InformacioniSistemBolnice.Commands
+{
+    public static class RoutedCommands
+    {
+        
+        public static RoutedUICommand StartingPage = new RoutedUICommand(
+            "Starting Page",
+            "StartingPage",
+            typeof(RoutedCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F1, ModifierKeys.None),
+            }
+            );
+        public static RoutedUICommand Patients = new RoutedUICommand(
+            "Patients",
+            "Patients",
+            typeof(RoutedCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F2, ModifierKeys.None),
+            }
+            );
+        public static RoutedUICommand Doctors = new RoutedUICommand(
+            "Doctors",
+            "Doctors",
+            typeof(RoutedCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F3, ModifierKeys.None),
+            }
+            );
+        public static RoutedUICommand Appointments = new RoutedUICommand(
+            "Appointments",
+            "Appointments",
+            typeof(RoutedCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4, ModifierKeys.None),
+            }
+            );
+        public static RoutedUICommand Reports = new RoutedUICommand(
+            "Reports",
+            "Reports",
+            typeof(RoutedCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F5, ModifierKeys.None),
+            }
+            );
+
+
+    }
+}
---
+++ InformacioniSistemBolnice/Secretary_ns/ReportsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InformacioniSistemBolnice.Secretary_ns
+{
+    /// <summary>
+    /// Interaction logic for ReportsPage.xaml
+    /// </summary>
+    public partial class ReportsPage : Page
+    {
+        private static ReportsPage _instance;
+        public static ReportsPage GetPage()
+        {
+            if (_instance == null)
+                _instance = new ReportsPage();
+            return _instance;
+        }
+        private ReportsPage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
