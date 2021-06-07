77c1c452b8b1b7f6fae2e5014d32e2164d57c1e9
bojictamara <bojic.ra79.2018@uns.ac.rs>
2021-06-07 19:09:47 +0200

    rad na izvestaju

---
+++ InformacioniSistemBolnice/Converter/RoomStatusConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InformacioniSistemBolnice.Converter
+{
+    class RoomStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value.GetType() == typeof(Boolean))
+            {
+                Boolean roomStatus = (Boolean)value;
+                if (roomStatus == true)
+                    return "Aktivna";
+                else if (roomStatus == false)
+                    return "Neaktivna";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
