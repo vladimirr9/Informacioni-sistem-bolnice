14db711a59e89cd1577e02ca3b60eefc8616a76c
bojictamara <bojic.ra79.2018@uns.ac.rs>
2021-06-10 17:59:53 +0200

    upravnik - feedback

---
+++ InformacioniSistemBolnice/Manager_ns/FeedbackWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using InformacioniSistemBolnice.Manager_ns.ViewModel;
+using InformacioniSistemBolnice.Upravnik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InformacioniSistemBolnice.Manager_ns
+{
+    /// <summary>
+    /// Interaction logic for FeedbackWindow.xaml
+    /// </summary>
+    public partial class FeedbackWindow : Window
+    {
+        public FeedbackWindow(UpravnikWindow parent)
+        {
+            InitializeComponent();
+            this.DataContext = new ManagerFeedbackViewModel(parent._loggedManager.Username, this);
+        }
+    }
+}
